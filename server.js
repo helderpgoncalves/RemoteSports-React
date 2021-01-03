1e6352d2852415e64676fdaab7e9ab355883042b
Hélder Gonçalves <helderpimentaxc@gmail.com>
2021-01-03 22:46:26 +0000

    commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 
 if(process.env.NODE_ENV === 'production' || true){
 	app.get('/*', (req, res) => {
-	  res.sendFile(path.join(__dirname+'/../client/build/index.html'));
+	  res.sendFile(path.join(__dirname+"client/build"));
   });
   }
 
