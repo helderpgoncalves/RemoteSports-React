df87dce7fed2e537484199996add26fc1df772d0
Hélder Gonçalves <helderpimentaxc@gmail.com>
2021-01-04 13:56:26 +0000

    commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const http = require('http')
+const http = require('https')
 var cors = require('cors')
 const app = express()
 const bodyParser = require('body-parser')
